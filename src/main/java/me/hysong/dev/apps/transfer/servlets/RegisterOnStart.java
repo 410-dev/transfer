280d9aee4b7b96c7d55f7d2f0f370d21402d5104
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-02-19 21:15:23 +0900

    RegisterOnStart: Temporarily removed due to deployment error

--- src/main/java/me/hysong/dev/apps/transfer/servlets/RegisterOnStart.java
+++ src/main/java/me/hysong/dev/apps/transfer/servlets/RegisterOnStart.java
@@ -1,25 +1,25 @@
-package me.hysong.dev.apps.transfer.servlets;
-
-import me.hysong.ApplicationMeta;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
-import java.util.ArrayList;
-
-@WebListener
-public class RegisterOnStart implements ServletContextListener {
-
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        ApplicationMeta meta = new ApplicationMeta(
-            "Transfer",
-            "{host}/transfer",
-            "Upload file to server temporarily to transfer files between devices.",
-            "transfer.png",
-            new ArrayList<>()
-        );
-        meta.save();
-    }
-
-}
+//package me.hysong.dev.apps.transfer.servlets;
+//
+//import me.hysong.ApplicationMeta;
+//
+//import javax.servlet.ServletContextEvent;
+//import javax.servlet.ServletContextListener;
+//import javax.servlet.annotation.WebListener;
+//import java.util.ArrayList;
+//
+//@WebListener
+//public class RegisterOnStart implements ServletContextListener {
+//
+//    @Override
+//    public void contextInitialized(ServletContextEvent sce) {
+//        ApplicationMeta meta = new ApplicationMeta(
+//            "Transfer",
+//            "{host}/transfer",
+//            "Upload file to server temporarily to transfer files between devices.",
+//            "transfer.png",
+//            new ArrayList<>()
+//        );
+//        meta.save();
+//    }
+//
+//}
